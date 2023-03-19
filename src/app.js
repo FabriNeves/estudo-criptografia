02241acd49b14bc2077052b6bb593851088b351c
FaSnow <eng.fbneves@gmail.com>
2023-03-19 01:11:26 -0300

    Criação dos metodos de login
    Teste de alguns metodos de login e autenticação

--- src/app.js
+++ src/app.js
@@ -1,20 +1,60 @@
 import express from "express";
 import loginRouter from "./routes/loginRoutes.js";
 import baseReponseModule from "./config/responseDefaultObj.js";
+import bodyParser from "body-parser";
 
 const app = express();
 
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
+app.use(express.json())
 app.use(loginRouter);
+
+
+//authentication
+app.post('/login', (req, res, next) => {
+    //esse teste abaixo deve ser feito no seu banco de dados
+    if(req.body.user === 'luiz' && req.body.password === '123'){
+      //auth ok
+      const id = 1; //esse id viria do banco de dados
+      const token = jwt.sign({ id }, process.env.SECRET, {
+        expiresIn: 300 // expires in 5min
+      });
+      return res.json({ auth: true, token: token });
+    }
+    
+    res.status(500).json({message: 'Login inválido!'});
+})
+
+app.post('/logout', function(req, res) {
+    res.json({ auth: false, token: null });
+})
 
 app.get("/", (req,res) =>{
 
-    console.log(req.headers);
-    const message = "Index";
-    const status = 200;
-    const isSuccess = true;
-    res.status(200).send(new baseReponseModule(message,isSuccess,status));
+
+    const response = new baseReponseModule(req.body,true,200);
+    res.status(200).send(response.toJSON);
 
 });
+
+function verifyJWT(req, res, next){
+    const token = req.headers['x-access-token'];
+    if (!token) return res.status(401).json({ auth: false, message: 'No token provided.' });
+    
+    jwt.verify(token, process.env.SECRET, function(err, decoded) {
+      if (err) return res.status(500).json({ auth: false, message: 'Failed to authenticate token.' });
+      
+      // se tudo estiver ok, salva no request para uso posterior
+      req.userId = decoded.id;
+      next();
+    });
+}
+
+app.get('/clientes', verifyJWT, (req, res, next) => { 
+    console.log("Retornou todos clientes!");
+    res.json([{id:1,nome:'luiz'}]);
+})
 
 // ---> Server.js
 export default app;
