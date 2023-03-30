b9d99d8bbc4f86959947be8c8fc0b4a5e9f2a7b7
FaSnow <eng.fbneves@gmail.com>
2023-03-30 14:38:12 -0300

    Exemplo de como ficaria em um banco não relacional

--- main.js
+++ main.js
@@ -1,20 +1,33 @@
-const jwt = require('jsonwebtoken');
+const a = {
+    idUser: 1,
+    email: "email@mail.com",
+    treinoA: [{       
+        exercicio: "Remada Curvada",
+        grupo: "Costas",
+        series: "4x12",
+        repeticoes: "12",
+        peso: "40kg",
+    }, {        
+        exercicio: "Box Jump",
+        grupo: "Perna",
+        series: "3x8",
+        repeticoes: "8",
+        peso: "80kg",
+    },
+    ],
+    treinoB: [{       
+        exercicio: "Remada Curvada",
+        grupo: "Costas",
+        series: "4x12",
+        repeticoes: "12",
+        peso: "40kg",
+    }, {        
+        exercicio: "Box Jump",
+        grupo: "Perna",
+        series: "3x8",
+        repeticoes: "8",
+        peso: "80kg",
+    },
+    ]
 
-// gerar um token JWT
-const token = jwt.sign({
-    id: 1,
-    username: 'Username',
-    body:"Mensagem do Corpo"
-},
-    'chave_secreta',
-    { expiresIn: '1h' }
-);
-
-// verificar um token JWT
-jwt.verify(token, 'chave_secreta', (err, decoded) => {
-    if (err) {
-        console.log('Token inválido');
-    } else {
-        console.log(decoded);
-    }
-});
+}
